4ab0beb21705814b8d3681f568ed1813e4d1c0ec
Amar-aj <amar.aj.github@gmail.com>
2023-01-27 17:57:22 +0530

    Add project files.

---
+++ mail-send-sendinblue/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+var apiKey = "YOUR_API_KEY";
+var client = new HttpClient();
+client.DefaultRequestHeaders.Add("api-key", apiKey);
+
+var data = new
+{
+    to = new[] { new { email = "recipient@example.com" } },
+    sender = new { email = "SENDER_EMAIL" },
+    subject = "Hello World",
+    htmlContent = "<p>Hello World</p>"
+};
+
+var json = JsonConvert.SerializeObject(data);
+var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+var response = await client.PostAsync("https://api.sendinblue.com/v3/smtp/email", content);
+
+if (response.IsSuccessStatusCode)
+{
+    Console.WriteLine("Email sent successfully");
+}
+else
+{
+    Console.WriteLine("Error sending email: " + await response.Content.ReadAsStringAsync());
+}
